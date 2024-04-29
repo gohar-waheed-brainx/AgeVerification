c2733171d07fba6bea629a88b7734f4266c0a6ad
gohar-waheed-brainx <163627848+gohar-waheed-brainx@users.noreply.github.com>
2024-04-29 14:06:37 +0500

    Fixed some issues

--- script.js
+++ script.js
@@ -8,8 +8,18 @@ function checkingCredentiels (event) {
         let currentDate = new Date();
         let currentYear = currentDate.getFullYear();
         var totalAge = currentYear - year;
-        if ((totalAge <= 15) && (dayInput.value >= currentDate.getDate())) {
+        if (totalAge < 15) {
             alert("Your age is Restricted");
+        } else if (totalAge === 15){
+            if (currentDate.getMonth() < monthInput.value - 1) {
+                alert("Your age is Restricted");
+            } else if ((currentDate.getMonth() === monthInput.value - 1) && (currentDate.getDate() <= dayInput.value)) {
+                alert("Your age is Restricted");
+            }
+            else {
+                event.preventDefault();
+                window.location.href = "https://www.google.com";    
+            }
         }
         else {
             event.preventDefault();
